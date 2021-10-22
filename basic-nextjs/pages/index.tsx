3778381a4c3f1cb46ec6308d77a2794f76e708e2
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-22 10:32:35 +0700

    video 8 - 10

--- basic-nextjs/pages/index.tsx
+++ basic-nextjs/pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <h1>Welcome ahmad fajar</h1>
+      <h1 className={styles["title-homepage"]}>Welcome ahmad fajar</h1>
+      <Footer />
     </>
   );
 };
