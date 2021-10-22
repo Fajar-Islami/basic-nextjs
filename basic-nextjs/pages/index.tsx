89de88270a759307e4ddd23683b4ead4e18fdb72
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-22 13:56:47 +0700

    video 13 - optimalisasi image

--- basic-nextjs/pages/index.tsx
+++ basic-nextjs/pages/index.tsx
@@ -1,12 +1,129 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <>
       <Layout pageTitle="Home Page">
+        <Image
+          src="/wallpaper_ts.png"
+          width={200}
+          height={200}
+          alt="TypeScript"
+        />
+        {/* <img src="/wallpaper_ts.png" style={{ width: 200, height: 200 }} /> */}
         <h1 className={styles["title-homepage"]}>Welcome ahmad fajar</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+
+        <Image
+          src="/wallpaper_js.png"
+          width={200}
+          height={200}
+          alt="Javascript"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+          provident excepturi, sapiente reprehenderit exercitationem dolor eaque
+          dolores asperiores laboriosam aliquam sed! Quam reprehenderit culpa
+          exercitationem repellendus possimus est! Voluptates, repudiandae?
+        </p>
+        <Image
+          src="/wallpaper_win8.jpg"
+          width={200}
+          height={200}
+          alt="Windows 8"
+        />
       </Layout>
     </>
   );
