8be1d10c0792fdac40b12afbd5217d4805256615
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-22 21:22:51 +0700

    Video 15 - getStaticProps

--- basic-nextjs/pages/users/index.tsx
+++ basic-nextjs/pages/users/index.tsx
@@ -1,9 +1,37 @@
+import { useEffect } from "react";
 import Layout from "../../components/Layout";
 
-export default function User() {
+interface UserProps {
+  dataUsers: any[];
+}
+
+export default function User(props: UserProps) {
+  const { dataUsers } = props;
+
+  console.log(dataUsers);
+
   return (
     <Layout pageTitle="User Page">
       <p>Users Page</p>
+      {dataUsers.map((user) => (
+        <>
+          <p>{user.name} </p>
+          <p>{user.email} </p>
+        </>
+      ))}
     </Layout>
   );
+}
+
+// Pemanggilan api di sisi server
+// Halaman ini sudah dibundle terlebih dahulu sebelum halamannya muncul
+// Tidak cocok untuk data dinamis
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    },
+  };
 }
